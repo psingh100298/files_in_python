e67b3791529f0a1b326b702c61554f6090b980be
Pradeep Singh <psingh10021998@gmail.com>
2023-05-01 22:15:37 +0530

    commit json files

--- json_imports.py
+++ json_imports.py
@@ -1,7 +1,23 @@
 import json
 
 file = open('friends_json.txt','r')
-file_content = json.loads(file)  #reads file and turns it to dictionary
+file_contents = json.load(file)   #reads file and turns it to dictionary
 
 file.close()
+
+print(file_contents['friends'][0])
+
+cars = [
+    {'make':'ford', 'model':'fiesta'},
+    {'make':'ford', 'model':'focus'}
+
+]
+
+file = open('cars_json.txt','w')
+json.dump(cars,file)
+file.close()
+
+my_json_string = '[{"name":"Alfa Romeo", "released":1950}]'
+incorrect_car = json.loads(my_json_string)
+print(incorrect_car[0]['name'])
 
